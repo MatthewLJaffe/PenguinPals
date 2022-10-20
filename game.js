b71c0e6a6e7205caef7e08bf29ed58ffda389be3
pkatherine <pkatherine@vt.edu>
2022-10-20 12:10:00 -0400

    scores

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ class gameObj{
       this.gameOver = false; //true if the game is over
       this.lose = false; //true if the player lost the game
       this.win = false;  //true if the player won the game
+      this.score = 0;
 
       //for menu in instructions screen
       this.cursor = 0;
@@ -12,7 +13,7 @@ class gameObj{
       this.cursorX = 280;
 
       //for high scores
-      this.highScores = [0, 0, 0, 0, 0];
+      this.highScores = [0, 0, 0, 0, 0];    //default high scores
 
       //for volume
       this.volume = 5;  //starts at 5
@@ -58,5 +59,10 @@ class gameObj{
             }
         }
     }
+
+    updateHighScores(){
+        
+    }
+
   
   }
